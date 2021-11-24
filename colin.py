1251a86e206a5255823b3ab7c976156591c4412b
Igor Korostil <eeghor@gmail.com>
2021-11-24 17:26:48 +1100

    initial floatables

--- colin.py
+++ colin.py
@@ -17,6 +17,22 @@ def is_currency(value_list: List[Any]):
     else:
         return False
 
+def is_amount(value_list: List[Any]):
+
+    total_floatable = 0
+
+    for s in value_list:
+        try:
+            total_floatable += (float(s) >= 0)
+        except:
+            pass
+
+    if total_floatable >= max(1, len(value_list)//2):
+        print(f'floatables: {total_floatable}/{len(value_list)}')
+        return True
+    else:
+        return False
+
 if __name__ == '__main__':
 
     list_ = ["$",'AUD', 0, 'RUB', 'midd', None, 'dsfds']
@@ -25,6 +41,10 @@ if __name__ == '__main__':
         print(f'list {list_} contains currencies')
     else:
         print(f'list {list_} has no currencies')
+
+    print(is_amount(list_))
+
+    print(is_amount([None, 1.132, 10, 35, "a"]))
 
 
 
